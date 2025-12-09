2d95ac3e180aab1ab8d8e4a993a2af8abf6c1d25
AngViera <mariana.viera@correounivalle.edu.co>
2025-12-09 14:10:46 -0500

    Update Establishments.jsx

--- src/pages/Establishments.jsx
+++ src/pages/Establishments.jsx
@@ -19,6 +19,7 @@ export default function Establishments() {
         </h2>
       </div>
 
+
       {/* Cards */}
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
         {establishments.map((item) => (
